f891bb7be0f1df85cdf24bb60ade0c02c7b2422f
flames of love <mechanicofthesequence@gmail.com>
2019-08-03 17:07:03 -0600

    better event emitting

--- mc-universe/mc-universe.cpp
+++ mc-universe/mc-universe.cpp
@@ -16,27 +16,67 @@ using WsServer = SimpleWeb::SocketServer<SimpleWeb::WS>;
 // using WsClient = SimpleWeb::SocketClient<SimpleWeb::WS>;
 
 // @Incomplete: Grid should be a shared_ptr: https://en.cppreference.com/w/cpp/memory/shared_ptr
-map<shared_ptr<WsServer::Connection>, shared_ptr<Grid>> connection_grid;
+unordered_map<shared_ptr<WsServer::Connection>, shared_ptr<Grid>> connection_grid;
 
+unordered_multimap<shared_ptr<WsServer::Connection>, string> subed_to;
+unordered_multimap<string, shared_ptr<WsServer::Connection>> subs;
 
 // api
-void emit(WsServer::Endpoint &event_emitter, string event, string data);
-
+void emit(string event, string data);
+void subscribe (shared_ptr<WsServer::Connection> conn, string event);
+void unsubscribe (shared_ptr<WsServer::Connection> conn, string event);
+void unsubscribe_all (shared_ptr<WsServer::Connection> conn);
 
 int main () {
     WsServer server;
     server.config.port = 1177;
 
-    // event emitter paths
-    auto &event_emitter = server.endpoint["^/event/([a-zA-Z0-9_-]+)/?$"];
+    // ==== EvENTS ====
+
+    auto &event_emitter = server.endpoint["^/events/?$"];
+    // event_emitter.on_open =
+    // [](shared_ptr<WsServer::Connection> conn) {}
+
+    event_emitter.on_close =
+    [](shared_ptr<WsServer::Connection> conn, int, const std::string &) {
+        unsubscribe_all(conn);
+    };
+
+    event_emitter.on_error =
+    [](shared_ptr<WsServer::Connection> conn, const error_code) {
+        unsubscribe_all(conn);
+    };
+
+    event_emitter.on_message =
+    [](shared_ptr<WsServer::Connection> conn, shared_ptr<WsServer::InMessage> msg) {
+        string str = msg->string();
+        auto len = str.length();
+        if (len < 2 || str[1] != ':') return; // do nothing
+        char cmd = str[0];
+        string event = str.substr(2, 255);
+        switch (cmd) {
+        case 'u': // unsubscribe an event
+            subscribe(conn, event);
+            break;
+
+        case 's': // subscribe to an event
+            unsubscribe(conn, event);
+            break;
+
+        case 'U': // unsubscribe to all events
+            unsubscribe_all(conn);
+            break;
+
+        default:
+            // unknown command
+            cout << "unknown command: '" << cmd << "' for event: " << event << endl;
+        }
+    };
+
+    // ==== GRID ====
 
     // https://www.regextester.com/97058
     auto &grid = server.endpoint["^/grid/([0-9]+)/([0-9]+)/([a-zA-Z0-9_-]+)/?$"];
-    grid.on_handshake =
-    grid.on_handshake = [](shared_ptr<WsServer::Connection> /*connection*/, SimpleWeb::CaseInsensitiveMultimap & /*response_header*/) {
-        cout << "handshake!" << endl;
-        return SimpleWeb::StatusCode::information_switching_protocols; // Upgrade to websocket
-    };
 
     grid.on_open =
     [](shared_ptr<WsServer::Connection> conn) {
@@ -67,7 +107,7 @@ int main () {
     };
 
     grid.on_message =
-    [&event_emitter](shared_ptr<WsServer::Connection> conn, shared_ptr<WsServer::InMessage> msg) {
+    [](shared_ptr<WsServer::Connection> conn, shared_ptr<WsServer::InMessage> msg) {
         cout << "grid(" << conn->path_match[1] << '|' << conn->path_match[2] << '|' << conn->path_match[3] << ").on_message" << endl;
         auto it = connection_grid.find(conn);
         assert(it != connection_grid.end());
@@ -93,7 +133,7 @@ int main () {
         // query the universe for nearest points to those values (TODO)
 
         // emit any events back to the client
-        emit(event_emitter, "test", "testing data");
+        emit("test", "testing data");
     };
 
     thread server_thread([&server]() { server.start(); });
@@ -109,10 +149,39 @@ int main () {
 }
 
 
-void emit(WsServer::Endpoint &event_emitter, string event, string data) {
-    for(auto &listener : event_emitter.get_connections()) {
-        if (listener->path_match[1] == event) {
-            listener->send(data);
-        }
+void emit(string event, string data) {
+    string msg = event + ':' + data;
+    auto it = subs.find(event);
+    if (it != subs.end()) {
+        it->second->send(msg);
+    }
+}
+
+void subscribe (shared_ptr<WsServer::Connection> conn, string event) {
+    subs.insert(make_pair(event, conn));
+    subed_to.insert(make_pair(conn, event));
+}
+
+void unsubscribe (shared_ptr<WsServer::Connection> conn, string event) {
+    for (auto it = subs.find(event); it != subs.end(); ) {
+        if (it->second == conn) subs.erase(it);
+        else it++;
+    }
+
+    for (auto it = subed_to.find(conn); it != subed_to.end(); ) {
+        if (it->second == event) subed_to.erase(it);
+        else it++;
+    }
+}
+
+void unsubscribe_all (shared_ptr<WsServer::Connection> conn) {
+    for (auto it = subs.begin(); it != subs.end(); ) {
+        if(it->second == conn) it = subs.erase(it);
+        else ++it;
+    }
+
+    for (auto it = subed_to.begin(); it != subed_to.end(); ) {
+        if(it->first == conn) it = subed_to.erase(it);
+        else ++it;
     }
 }
